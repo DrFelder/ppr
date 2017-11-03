f2e911fdb77020a08110f5a7c0dfe0aaa821d035
Thomas Pötzsch <poetzsch@punkt.de>
2017-11-03 11:10:26 +0100

    Added JUnit 5 compatability and basic web.xml

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class FirstTest {
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 2);
+    }
+}
