5bffe528a4b2eeb22ebc9832e71afc6ce28a6d43
Thomas Pötzsch <github@surreal.is>
2017-12-10 04:29:17 +0100

    PPR-90 Added equipment and helpers list to the operation detail page, updated tests

--- src/main/java/is/surreal/ppr/controller/DetailController.java
+++ src/main/java/is/surreal/ppr/controller/DetailController.java
@@ -18,7 +18,11 @@
 
 package is.surreal.ppr.controller;
 
+import is.surreal.ppr.model.Equipment;
+import is.surreal.ppr.model.Helper;
 import is.surreal.ppr.model.Operation;
+import is.surreal.ppr.repository.EquipmentRepository;
+import is.surreal.ppr.repository.HelperRepository;
 import is.surreal.ppr.repository.OperationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -27,6 +31,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class DetailController {
@@ -34,10 +39,20 @@ public class DetailController {
     @Autowired
     private OperationRepository operationRepository;
 
+    @Autowired
+    private EquipmentRepository equipmentRepository;
+
+    @Autowired
+    private HelperRepository helperRepository;
+
     @RequestMapping(value = "/detail")
     public ModelAndView detail(ModelAndView model, @RequestParam Long id) throws IOException {
         Operation operation = operationRepository.findOne(id);
         model.addObject("operation", operation);
+        List<Equipment> equipmentList = equipmentRepository.findAll();
+        model.addObject("equipmentList", equipmentList);
+        List<Helper> helperList = helperRepository.findAll();
+        model.addObject("helperList", helperList);
         model.setViewName("detail");
 
         return model;
