f42d3596742cf56d500922a2dcefca9791a99144
Thomas Pötzsch <github@surreal.is>
2017-12-10 17:44:08 +0100

    PPR-92 Add link to apply to requirements

--- src/main/java/is/surreal/ppr/controller/ApplicationController.java
+++ src/main/java/is/surreal/ppr/controller/ApplicationController.java
@@ -1,25 +1,36 @@
 package is.surreal.ppr.controller;
 
-import is.surreal.ppr.model.Operation;
-import is.surreal.ppr.repository.OperationRepository;
+import is.surreal.ppr.model.Operationparticipation;
+import is.surreal.ppr.repository.OperationparticipationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ApplicationController {
 
     @Autowired
-    OperationRepository operationRepository;
+    OperationparticipationRepository operationparticipationRepository;
 
-    @PostMapping("/applyforoperation")
-    public String applyForRequirement(@ModelAttribute Operation operation, RedirectAttributes redirectAttributes) {
-        operation.setOrganizerId(101);
-        redirectAttributes.addAttribute("id", operation.getId());
+    @RequestMapping("/applyforhelper")
+    public String applyForHelper(@RequestParam int userId, @RequestParam int helperId, @RequestParam int operationId, RedirectAttributes redirectAttributes) {
+        Operationparticipation operationparticipation = new Operationparticipation();
+        operationparticipation.setUserId(userId);
+        operationparticipation.setHelperId(helperId);
+        operationparticipationRepository.save(operationparticipation);
+        redirectAttributes.addAttribute("id", operationId);
+        return "redirect:/detail";
+    }
+
+    @RequestMapping("/applyforequipment")
+    public String applyForEquipment(@RequestParam int userId, @RequestParam int equipmentId, @RequestParam int operationId, RedirectAttributes redirectAttributes) {
+        Operationparticipation operationparticipation = new Operationparticipation();
+        operationparticipation.setUserId(userId);
+        operationparticipation.setEquipmentId(equipmentId);
+        operationparticipationRepository.save(operationparticipation);
+        redirectAttributes.addAttribute("id", operationId);
         return "redirect:/detail";
     }
 
