39e62d9aafbf04353df3034347f1d6c06f87a099
Stephan <stephanpf@gmail.com>
2018-06-12 12:34:33 +0200

    PPR-105 Add Bulma CSS Framework

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import store from './store';
+import './../node_modules/bulma/css/bulma.css';
 
 require('./assets/css/main.css');
 
